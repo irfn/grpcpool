58441cdb1b39a02d28c52da98fc62bc2d03dba48
Manohar Akula <manohar.akula@go-jek.com>
2017-08-30 12:39:46 +0530

    add evict function to evict and destroy connection from pool

--- connection_test.go
+++ connection_test.go
@@ -6,15 +6,14 @@ import (
 	"google.golang.org/grpc"
 
 	"github.com/stretchr/testify/mock"
+	"context"
+	"time"
+	"net"
 )
 
 type MockPool struct {
 	mock.Mock
 	mockConnection Connection
-}
-
-type MockConnection struct {
-	mock.Mock
 }
 
 func (m MockPool) Get() (Connection, error) {
@@ -33,7 +32,32 @@ func (m MockPool) put(client *grpc.ClientConn) error {
 func TestShouldPutConnectionBackInPoolAfterClose(t *testing.T) {
 	mockPool := new(MockPool)
 
-	connection := &GrpcConnection{pool: mockPool, GrpcConn: &grpc.ClientConn{}}
-	mockPool.On("put", connection.GrpcConn).Return(nil)
-	connection.Close()
+	mockConnection := &MockConnection{}
+	conn, _ := grpc.DialContext(context.Background(), "fakeaddr", grpc.WithDialer(func(string, time.Duration) (net.Conn, error) {
+		return mockConnection, nil
+	}), grpc.WithInsecure())
+
+	grpcConnection := &GrpcConnection{pool: mockPool, GrpcConn: conn}
+	mockPool.On("put", grpcConnection.GrpcConn).Return(nil)
+
+	grpcConnection.Close()
+
+	mockConnection.AssertNotCalled(t, "Close")
+}
+
+func TestShouldPutEvictConnectionFromPool(t *testing.T) {
+	mockPool := new(MockPool)
+
+	mockConnection := &MockConnection{}
+	conn, _ := grpc.DialContext(context.Background(), "fakeaddr", grpc.WithDialer(func(string, time.Duration) (net.Conn, error) {
+		return mockConnection, nil
+	}), grpc.WithInsecure())
+
+	grpcConnection := &GrpcConnection{pool: mockPool, GrpcConn: conn}
+	mockPool.On("put", grpcConnection.GrpcConn).Return(nil)
+	mockConnection.On("Close").Return(nil)
+
+	grpcConnection.Evict()
+
+	mockConnection.AssertExpectations(t)
 }
